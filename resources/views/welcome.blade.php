c216e7a2b818298501c9a6eb36c468e1f195d5c8
hisashi.takagi <worldhistory14@gmail.com>
2019-10-23 14:28:31 +0000

    delete welcome image

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,16 +3,6 @@
 @section('content')
     @if (Auth::check())
         <h1>タスク一覧</h1>
-        <aside class="col-sm-4">
-            <div class="card">
-                <div class="card-header">
-                    <h3 class="card-title">{{ Auth::user()->name }}</h3>
-                </div>
-                <div class="card-body">
-                    <img class="rounded img-fluid" src="{{ Gravatar::src(Auth::user()->email, 500) }}" alt="">
-                </div>
-            </div>
-        </aside>
         @include('tasks.tasks', ['tasks' => $tasks])
     @else
         <div class="center jumbotron">
